ce74055cde7b4d3a21d367d545927a4f8f3acc83
hqs <huangqingsong_5678@hotmail.com>
2016-03-06 21:57:50 +0800

    initial project

---
+++ testRoundImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testRoundImage
+//
+//  Created by 火星人 on 16/3/6.
+//  Copyright © 2016年 火星人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
